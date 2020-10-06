b4ebd9937c7897ac82882c8c19f1349ebebc0194
Aditya Gupta <adityag1998@gmail.com>
2020-10-06 14:02:21 +0530

    Changed behaviour of DummyCamera to mimick NaviCam
    1. create bundle and insert using put serializable
    2. bundle has objectList and textString
    3. name of actions and keys have also been changed in accordance to actual camera app

--- app/src/main/java/com/example/dummycamera/MainActivity.java
+++ app/src/main/java/com/example/dummycamera/MainActivity.java
@@ -3,26 +3,83 @@ package com.example.dummycamera;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.Arrays;
 
-    protected String objectName;
-    protected EditText intentInput;
+public class MainActivity extends AppCompatActivity {
+    // Class functions
+    public static boolean isPackageInstalled(String packageName, PackageManager packageManager) {
+        try {
+            return packageManager.getApplicationInfo(packageName, 0).enabled;
+        }
+        catch (PackageManager.NameNotFoundException e) {
+            return false;
+        }
+    }
+
+    // Class Members
+    protected static final String RECEIVE_OBJECT_NAME = "com.samsung.navicam.parse_object_text_bundle";
+    protected static final String KEY1 = "com.samsung.navicam.ObjectList";
+    protected static final String KEY2 = "com.samsung.navicam.text";
+    protected static final String BENEFICIARY = "com.samsung.smartnotes";
+
+    // Data Members
+    protected EditText intentInputText;
+    protected EditText intentInputObjectList;
     protected Button sendButton;
-    protected static final String RECEIVE_OBJECT_NAME = "com.example.dummycamera.receive_object_name";
-    protected static final String KEY = "com.example.dummycamera.objectName";
+
+    // Helper Functions
+    protected void showToast(ArrayList<String> objectList, String text) {
+        String toastText = "Broadcast is Fired: " + "\n" +
+                "Object List Size: " + objectList.size() + "\n" +
+                "Text String Length: " + text.length();
+        Toast.makeText(this, toastText, Toast.LENGTH_SHORT).show();
+    }
+
+    protected void showFailToast(){
+        Toast.makeText(this, "Companion App: com.samsung.smartnotes Not Found", Toast.LENGTH_SHORT).show();
+    }
+
+    protected Bundle createBundle(ArrayList<String> objectList, String text){
+        Bundle bundle = new Bundle();
+        bundle.putSerializable(KEY1, objectList);
+        bundle.putSerializable(KEY2, text);
+        return bundle;
+    }
+
+    protected String getText(EditText intentInputText) {
+        return intentInputText.getText().toString();
+    }
+
+    protected ArrayList<String> getObjectList(EditText intentInputObjectList) {
+        String processableString = intentInputObjectList.getText().toString();
+        String[] processedString = processableString.split(",");
+        return new ArrayList<>(Arrays.asList(processedString));
+    }
+
+    protected void sendBroadcastIntent (Bundle bundle) {
+        Intent intent = new Intent();
+        intent.setAction(RECEIVE_OBJECT_NAME);
+        intent.addCategory("android.intent.category.DEFAULT");
+        intent.setPackage("com.samsung.smartnotes");
+        intent.putExtras(bundle);
+        sendBroadcast(intent);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        intentInput = (EditText) findViewById(R.id.intentInput);
+        intentInputObjectList = (EditText) findViewById(R.id.intentInputObjectList);
+        intentInputText = findViewById(R.id.intentInputText);
         sendButton = (Button) findViewById(R.id.sendButton);
     }
 
@@ -33,24 +90,16 @@ public class MainActivity extends AppCompatActivity {
         sendButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                objectName = intentInput.getText().toString();
-                if (objectName.length() == 0) {
-                    showToast("Please Enter Object Name to Send");
+                if (MainActivity.isPackageInstalled(BENEFICIARY, getPackageManager())) {
+                    ArrayList<String> objectList = getObjectList(intentInputObjectList);
+                    String text = getText(intentInputText);
+                    sendBroadcastIntent(createBundle(objectList, text));
+                    showToast(objectList, text);
                 }
                 else {
-                    Intent intent = new Intent();
-                    intent.setAction(RECEIVE_OBJECT_NAME); //Custom Action
-                    intent.addCategory("android.intent.category.DEFAULT");
-                    intent.setPackage("com.samsung.smartnotes");
-                    intent.putExtra(KEY, objectName);
-                    sendBroadcast(intent);
-                    showToast("Broadcast is Fired with Object Name : " + objectName);
+                    showFailToast();
                 }
             }
         });
-    }
-
-    protected void showToast(String text) {
-        Toast.makeText(MainActivity.this, text, Toast.LENGTH_SHORT).show();
     }
 }
